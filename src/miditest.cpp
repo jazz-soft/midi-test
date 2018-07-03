3c80c93db1859ac542ea15e255ec7f45c9f2f2d9
Sema <sema@pyva.net>
2018-07-02 23:02:21 -0400

    Constructors

--- src/miditest.cpp
+++ src/miditest.cpp
@@ -1,28 +1,92 @@
 #include <node_api.h>
+#include <iostream>
+#include "miditest.h"
 
-namespace demo {
+namespace miditest {
 
-napi_value Method(napi_env env, napi_callback_info args) {
-  napi_value greeting;
+napi_ref MidiSrc_ctor;
+napi_ref MidiDst_ctor;
+
+napi_value echo(napi_env env, napi_callback_info args) {
+  size_t argc = 1;
+  napi_value argv[1];
+  napi_value self;
   napi_status status;
+  status = napi_get_cb_info(env, args, &argc, argv, &self, 0);
+  return argv[0];
+}
 
-  status = napi_create_string_utf8(env, "hello", NAPI_AUTO_LENGTH, &greeting);
-  if (status != napi_ok) return nullptr;
-  return greeting;
+napi_value MidiSrc(napi_env env, napi_callback_info args) {
+  size_t argc = 1;
+  napi_value argv[1];
+  napi_value self;
+  napi_value ctor;
+  bool bbb;
+  napi_status status;
+  status = napi_get_cb_info(env, args, &argc, argv, &self, 0);
+  if (status != napi_ok) return 0;
+
+  status = napi_get_reference_value(env, MidiSrc_ctor, &ctor);
+  if (status != napi_ok) return 0;
+
+  status = napi_instanceof(env, self, ctor, &bbb);
+  if (status != napi_ok) return 0;
+  if (!bbb) {
+    status = napi_new_instance(env, ctor, argc, argv, &self);
+    return status == napi_ok ? self : 0;
+  }
+
+  return self;
+}
+
+napi_value MidiDst(napi_env env, napi_callback_info args) {
+  size_t argc = 1;
+  napi_value argv[1];
+  napi_value self;
+  napi_value ctor;
+  bool bbb;
+  napi_status status;
+  status = napi_get_cb_info(env, args, &argc, argv, &self, 0);
+  if (status != napi_ok) return 0;
+
+  status = napi_get_reference_value(env, MidiDst_ctor, &ctor);
+  if (status != napi_ok) return 0;
+
+  status = napi_instanceof(env, self, ctor, &bbb);
+  if (status != napi_ok) return 0;
+  if (!bbb) {
+    status = napi_new_instance(env, ctor, argc, argv, &self);
+    return status == napi_ok ? self : 0;
+  }
+
+  return self;
 }
 
 napi_value init(napi_env env, napi_value exports) {
   napi_status status;
-  napi_value fn;
+  napi_value ctor;
 
-  status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
-  if (status != napi_ok) return nullptr;
+  napi_property_descriptor props[] = {
+    { "echo", NULL, echo, NULL, NULL, NULL, napi_enumerable, NULL }
+  };
 
-  status = napi_set_named_property(env, exports, "hello", fn);
-  if (status != napi_ok) return nullptr;
+  status = napi_define_class(env, "MidiSrc", NAPI_AUTO_LENGTH, MidiSrc, NULL, sizeof props / sizeof props[0], props, &ctor);
+  if (status != napi_ok) return 0;
+  status = napi_create_reference(env, ctor, 1, &MidiSrc_ctor);
+  if (status != napi_ok) return 0;
+  status = napi_set_named_property(env, exports, "MidiSrc", ctor);
+  if (status != napi_ok) return 0;
+
+  status = napi_define_class(env, "MidiDst", NAPI_AUTO_LENGTH, MidiDst, NULL, sizeof props / sizeof props[0], props, &ctor);
+  if (status != napi_ok) return 0;
+  status = napi_create_reference(env, ctor, 1, &MidiDst_ctor);
+  if (status != napi_ok) return 0;
+  status = napi_set_named_property(env, exports, "MidiDst", ctor);
+  if (status != napi_ok) return 0;
+
   return exports;
 }
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
 
-}  // namespace demo
+}  // namespace miditest
--- test.js
+++ test.js
@@ -1,2 +1,16 @@
-const test = require('.');
-console.log(test);
+const mt = require('.');
+console.log('mt:', mt);
+console.log('mt.MidiSrc:', mt.MidiSrc);
+console.log('mt.MidiDst:', mt.MidiDst);
+
+var src = new mt.MidiSrc();
+console.log('new MidiSrc():', src, src.echo, src.echo('src'));
+
+var sss = mt.MidiSrc();
+console.log('MidiSrc():', sss, sss.echo, sss.echo('sss'));
+
+var dst = new mt.MidiDst();
+console.log('new MidiDst():', dst, dst.echo, dst.echo('dst'));
+
+var ddd = mt.MidiDst();
+console.log('MidiDst():', ddd, ddd.echo, ddd.echo('ddd'));
