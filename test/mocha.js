421feb860639a4a370df037bcd40dd521ece8552
Sema <sema@pyva.net>
2018-07-14 01:09:42 -0400

    Fixed test

--- test/mocha.js
+++ test/mocha.js
@@ -3,12 +3,18 @@ var mt = require('..');
 var JZZ = require('jzz');
 
 describe('MIDI messages', function() {
-  it('jzz', function() {
-    JZZ().and(function(){ console.log(this.info()); });
+  it('jzz', function(done) {
+    JZZ().and(function() {
+      console.log(this.info());
+      done();
+    });
   });
-  it('virtual-midi-in', function() {
+  it('virtual-midi-in', function(done) {
     var port = mt.MidiSrc('Virtual MIDI-In');
     port.connect();
-    JZZ().and(function(){ console.log(this.info()); });
+    JZZ().refresh().and(function() {
+      console.log(this.info());
+      done();
+    });
   });
 });
