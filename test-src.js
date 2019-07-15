274092bd27af25d0f79aece4ae83523c51ec6eb1
Sema <sema@pyva.net>
2019-07-14 23:35:55 -0400

    Updated script

--- test-src.js
+++ test-src.js
@@ -1,18 +1,27 @@
+var readline = require('readline');
 const mt = require('.');
-var send = [ [0x90, 0x40, 0x7f], [0x80, 0x40, 0x00] ];
-var count = 0;
 var src = mt.MidiSrc('VIRTUAL MIDI-In');
 console.log('Opening ' + src.name + ':', src.connect());
-console.log('Press <Enter> to continue...');
-process.stdin.on('data', function(s) {
-  if (count < send.length) {
-    console.log('Sending:', send[count]);
-    src.emit(send[count]);
-    count++;
+console.log('Enter MIDI sequence or Ctrl-C to terminate...');
+var rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.on('line', function(line){
+  var data = line.split(/\s+/);
+  var msg = [];
+  var i, x;
+  for (i = 0; i < data.length; i++) {
+    if (data[i] == '') continue;
+    x = parseInt(data[i], 16);
+    if (x >= 0 && x <= 255) msg.push(x);
+    else {
+      console.log('Invalid hex MIDI value:', data[i]);
+      return;
+    }
   }
-  else {
-    console.log('Closing ' + src.name + ':', src.disconnect());
-    console.log('Done!');
-    process.exit(0);
+  if (!msg.length) return;
+  data = [];
+  for (i = 0; i < msg.length; i++) {
+    data.push(('0' + msg[i].toString(16)).substr(-2));
   }
+  console.log('Sending:', data.join(' '));
+  src.emit(msg);
 });
